e5a853da75118a9ce2cf6a5194d823aeee0b66a4
Brigita <97016501+brimac2@users.noreply.github.com>
2022-03-14 09:19:03 +0200

    fix/delete packages
    deleted not important for this task packages

--- src/com/itacademy/java/oop/basics/task3/AtmApplication.java
+++ src/com/itacademy/java/oop/basics/task3/AtmApplication.java
@@ -3,9 +3,10 @@ package com.itacademy.java.oop.basics.task3;
 public class AtmApplication {
     public static void main(String[] args) {
         MyBankAtm brigitaBank = new MyBankAtm(1000);
-        CreditCard creditCard = new CreditCard(0, "Brigita", "1234123412341234", 10, 1000);
+        CreditCard creditCard = new CreditCard(-90, "Brigita", "1234123412341234", 10, 1000);
         DebitCard debitCard = new DebitCard(120, "Martynas", "3213213213213212");
 
+        brigitaBank.addFunds(100, creditCard);
         brigitaBank.addFunds(100, debitCard);
 
 
